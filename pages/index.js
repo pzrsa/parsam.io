042c7c5acb0463a7e46eb47a965a37182a103ea7
parsa <parsamesg@gmail.com>
2021-11-01 17:08:55 +0000

    add profile pic with greeting

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
+import Image from "next/image";
+import styles from "../styles/modules/Home.module.css";
 import utilStyles from "../styles/utils.module.css";
 
 const Home = () => {
   return (
     <div>
-      <header>
-        <h1 className={utilStyles.headingXl}>👋 Hey, I&apos;m Parsa.</h1>
-        <p className={utilStyles.lightText}>
-          Software Development Apprentice at Google{" "}
-        </p>
+      <header className={styles.header}>
+        <Image
+          priority
+          className={utilStyles.borderCircle}
+          src="https://avatars.githubusercontent.com/u/76453314"
+          height={144}
+          width={144}
+          alt="Parsa"
+        />
+        <div className={styles.greeting}>
+          <h1 className={utilStyles.headingXl}>👋 Hey, I&apos;m Parsa.</h1>
+          <p className={utilStyles.lightText}>
+            Software Development Apprentice at Google{" "}
+          </p>
+        </div>
       </header>
     </div>
   );
