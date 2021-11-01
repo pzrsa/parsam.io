c8af3b9e2ac282ad3865f3336a119995949ab2ed
parsa <parsamesg@gmail.com>
2021-11-01 20:36:11 +0000

    get sample article front matter data, and display it on the page temporarily

--- pages/articles.js
+++ pages/articles.js
@@ -1,11 +1,34 @@
+import { getSortedArticlesData } from "../lib/articles";
 import utilStyles from "../styles/utils.module.css";
 
-const Articles = () => {
+export const getStaticProps = () => {
+  const allArticlesData = getSortedArticlesData();
+  return {
+    props: {
+      allArticlesData,
+    },
+  };
+};
+
+const Articles = ({ allArticlesData }) => {
   return (
     <div>
       <header>
         <h1 className={utilStyles.heading2Xl}>Articles</h1>
       </header>
+      <section>
+        <ul className={utilStyles.list}>
+          {allArticlesData.map(({ slug, date, title }) => (
+            <li className={utilStyles.listItem} key={slug}>
+              {title}
+              <br />
+              {slug}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
