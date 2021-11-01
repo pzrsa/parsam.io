c95aaaa8b7bf4d9583327fde592f8394e1ee8cdc
parsa <parsamesg@gmail.com>
2021-11-01 21:38:52 +0000

    use full name for title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,14 +2,13 @@ import Head from "next/head";
 import styles from "../styles/modules/Layout.module.css";
 import Navbar from "./Navbar";
 
-export const siteTitle = "parsa";
+export const siteTitle = "Parsa Mesgarha";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <Head>
         <meta property="og:image" content="/images/avatar.jpg" />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="og:title" content={siteTitle} />
       </Head>
