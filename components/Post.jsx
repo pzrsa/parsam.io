532451c150ca1651f1e2d16f42d08eb479bf0dde
parsa <parsamesg@gmail.com>
2021-11-02 09:22:31 +0000

    display post on dynamic route

---
+++ components/Post.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Date from "./Date";
+
+const Post = ({ postData }) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <header>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+      </header>
+      <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+  );
+};
+
+export default Post;
