96ea5556ce68405bde37cf086ec89126fa6b9a5b
parsa <parsamesg@gmail.com>
2021-11-03 15:58:12 +0000

    add seo stuff

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,17 +1,32 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import styles from "../styles/modules/Layout.module.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export const siteTitle = "Parsa Mesgarha";
-
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  const title = "Parsa Mesgarha";
+  const description = "Parsa Mesgarha's website about tech, books and life.";
+  const image = "/images/avatar.jpg";
+
   return (
     <div className={styles.container}>
       <Head>
-        <meta property="og:image" content="/images/avatar.jpg" />
+        <meta name="robots" content="follow, index" />
+        <meta content={description} name="description" />
+        <meta property="og:url" content={`https://parsam.io${router.asPath}`} />
+        <link rel="canonical" href={`https://parsam.io${router.asPath}`} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:image" content={image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:site" content="@_parsam" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Head>
       <Navbar />
       <main>{children}</main>
