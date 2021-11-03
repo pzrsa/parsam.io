966a09b1049690faeabd0b4dda21fc9bff11b81c
parsa <parsamesg@gmail.com>
2021-11-03 22:34:45 +0000

    custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+
+const Custom404 = () => {
+  return (
+    <div>
+      <Head>
+        <title>404 - Parsa Mesgarha</title>
+      </Head>
+      <h1 style={{ textAlign: "center" }} className={utilStyles.heading2Xl}>
+        404 - Page not found
+      </h1>
+    </div>
+  );
+};
+
+export default Custom404;
