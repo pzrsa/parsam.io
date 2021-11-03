593a0aa7fedc8dcea5957973895404fdbfabd712
parsa <parsamesg@gmail.com>
2021-11-03 15:46:44 +0000

    add favicon stuff

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,10 +6,30 @@ class ParsaDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://avatars.githubusercontent.com/u/76453314"
-            rel="shortcut icon"
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
           />
-          <meta name="description" content="Parsa's Personal Website." />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#111" />
         </Head>
         <body>
           <Main />
