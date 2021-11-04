c7eadf9f7acd0aa964d12074a7aac0d66be016c7
parsa <parsamesg@gmail.com>
2021-11-04 09:12:25 +0000

    update footer with source of site

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+import { VscSourceControl } from "react-icons/vsc";
 import styles from "../styles/modules/Footer.module.css";
 
 const FooterItem = ({ href, icon }) => {
@@ -19,6 +20,10 @@ const Footer = () => {
     <footer className={styles.footer}>
       <FooterItem href="https://twitter.com/_parsam" icon={<SiTwitter />} />
       <FooterItem href="https://github.com/pzrsa" icon={<SiGithub />} />
+      <FooterItem
+        href="https://github.com/pzrsa/parsam.io"
+        icon={<VscSourceControl />}
+      />
       <FooterItem
         href="https://www.linkedin.com/in/parsamesgarha/"
         icon={<SiLinkedin />}
