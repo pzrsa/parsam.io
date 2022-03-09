c20d6b57c4dacc59248183d4f7e6e1ed0dc20465
parsa <parsamesg@gmail.com>
2022-03-09 21:52:26 +0000

    twitter and opengraph stuff for each post instead of the same title and description

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import styles from "../styles/modules/Post.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Date from "./Date";
+import { useRouter } from "next/router";
 
 interface PostProps {
   postType: string;
@@ -15,10 +16,20 @@ interface PostProps {
 }
 
 const Post: React.FC<PostProps> = ({ postType, postData }) => {
+  const router = useRouter();
   return (
     <div>
       <Head>
         <title>{postData.title} - Parsa Mesgarha</title>
+        <meta name="robots" content="all" />
+        <meta content={postData.title} name="description" />
+        <meta property="og:site_name" content={postData.title} />
+        <meta property="og:description" content={postData.title} />
+        <meta property="og:title" content={postData.title} />
+        {/*<meta property="og:image" content={image} />*/}
+        <meta name="twitter:title" content={postData.title} />
+        <meta name="twitter:description" content={postData.title} />
+        {/*<meta name="twitter:image" content={image} />*/}
       </Head>
       <header>
         <h1 className={utilStyles.heading2Xl}>{postData.title}</h1>
