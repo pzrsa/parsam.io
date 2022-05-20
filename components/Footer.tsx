266a4fa63c11b4e891458bf1bed3eb0aeddf3304
parsa <parsamesg@gmail.com>
2022-05-20 17:52:54 +0100

    update footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import { ReactElement } from "react";
 import { SiGithub, SiLinkedin, SiTwitter, SiYoutube } from "react-icons/si";
 import { VscSourceControl } from "react-icons/vsc";
-import styles from "../styles/modules/Footer.module.css";
 import ThemeToggle from "./ThemeToggle";
 
 interface FooterProps {
@@ -12,7 +11,7 @@ interface FooterProps {
 const FooterItem: React.FC<FooterProps> = ({ href, icon }) => {
   return (
     <a
-      className={styles.item}
+      className="text-2xl hover:text-neutral-500 dark:hover:text-neutral-400 transition-all "
       href={href}
       rel="prefetch noreferrer"
       target="_blank"
@@ -24,7 +23,7 @@ const FooterItem: React.FC<FooterProps> = ({ href, icon }) => {
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className="mt-10 flex gap-3 items-center">
       <FooterItem
         href="https://twitter.com/_parsam"
         icon={<SiTwitter title="Twitter" />}
@@ -45,7 +44,7 @@ const Footer = () => {
         href="https://www.linkedin.com/in/parsamesgarha/"
         icon={<SiLinkedin title="LinkedIn" />}
       />
-      <span style={{ flexGrow: 1 }} />
+      <span className="flex-grow" />
       <ThemeToggle />
     </footer>
   );
