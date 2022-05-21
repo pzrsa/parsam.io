86290fbe0f67e1e54bd7f214015e5339e6ed5eea
parsa <parsamesg@gmail.com>
2022-05-21 14:24:10 +0100

    create cards for displaying book notes with images

---
+++ components/NoteCards.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Notes } from "../lib/types";
+import Date from "./Date";
+
+interface NoteCardsProps {
+  notes: Notes;
+}
+
+const NoteCards: React.FC<NoteCardsProps> = ({ notes }) => {
+  return (
+    <div className="mx-auto">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2">
+        {notes.map((note) => (
+          <Link key={note.slug} href={`/notes/${note.slug}`}>
+            <a className="group">
+              <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg cursor-pointer">
+                <Image
+                  alt={note.title}
+                  src={note.image}
+                  layout="fill"
+                  className="group-hover:opacity-75 transition-all"
+                />
+              </div>
+
+              <div className="group-hover:text-neutral-500 dark:group-hover:text-neutral-400 transition-all">
+                <h2 className="mt-2 text-xl sm:text-2xl font-bold">
+                  {note.title}
+                </h2>
+                <p className="text-neutral-600 dark:text-neutral-400">
+                  by {note.author} - <Date dateString={note.date} />
+                </p>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NoteCards;
--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,8 +1,9 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import NoteCards from "../components/NoteCards";
 import PageTitle from "../components/PageTitle";
-import Posts from "../components/Posts";
 import { getSortedNotesData } from "../lib/notes";
+import { Notes } from "../lib/types";
 
 export const getStaticProps: GetStaticProps = () => {
   const allNotesData = getSortedNotesData();
@@ -14,7 +15,7 @@ export const getStaticProps: GetStaticProps = () => {
 };
 
 interface NotesProps {
-  allNotesData: [{ slug: string; title: string; date: string; author: string }];
+  allNotesData: Notes;
 }
 
 const Notes: React.FC<NotesProps> = ({ allNotesData }) => {
@@ -24,7 +25,7 @@ const Notes: React.FC<NotesProps> = ({ allNotesData }) => {
         <title>Book Notes - Parsa Mesgarha</title>
       </Head>
       <PageTitle name="Book Notes" />
-      <Posts postType="notes" itemsData={allNotesData} />
+      <NoteCards notes={allNotesData} />
     </>
   );
 };
