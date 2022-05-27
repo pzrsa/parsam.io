ac3a9e12cbda4de811ff3b9884abdd723dddcf69
parsa <parsamesg@gmail.com>
2022-05-28 00:14:36 +0100

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -34,7 +34,7 @@ const About: React.FC = ({}) => {
       <Head>
         <title>About Me - Parsa Mesgarha</title>
       </Head>
-      <div>
+      <div className="prose dark:prose-invert">
         <PageTitle name="About Me" />
         <div className="relative sm:flex sm:flex-row-reverse">
           <div className="mx-auto w-60 h-60 sm:w-full sm:h-full ">
@@ -53,115 +53,114 @@ const About: React.FC = ({}) => {
             </p>
           </div>
         </div>
-      </div>
-      <div>
-        <h2 className="text-2xl font-bold mb-3 mt-8">Things I like</h2>
-        <ul className="prose dark:prose-invert list-disc list-outside">
-          <li>
-            <a
-              href="https://github.com/pzrsa?tab=stars"
-              rel="prefetch noreferrer"
-              target="_blank"
-            >
-              Open source projects
-            </a>
-            . It&apos;s fascinating that random people all over the internet are
-            able to come together to change the world.
-          </li>
-          <li>
-            Reading non-fiction and fiction books on my Kindle. Currently
-            reading{" "}
-            <a
-              href="https://en.wikipedia.org/wiki/The_Obstacle_Is_the_Way"
-              rel="prefetch noreferrer"
-              target="_blank"
-            >
-              The Obstacle Is the Way
-            </a>{" "}
-            by Ryan Holiday.
-          </li>
-          <li>
-            Mechanical keyboards. I love the fact that I could build something
-            with parts tailored to my preference. I use a keyboard daily, so it
-            makes sense for me to invest in tools that inspire me to use them
-            more.
-          </li>
-          <li>
-            Doing typing tests on{" "}
-            <a
-              href="https://monkeytype.com/"
-              rel="prefetch noreferrer"
-              target="_blank"
-            >
-              monkeytype
-            </a>
-            . I&apos;ve now thankfully moved over to using 9 fingers for typing
-            after only using 3 previously. Wasn&apos;t easy but 100% worth the
-            frustration in the beginning. My current highest WPM is 116 (95%
-            accuracy).
-          </li>
-          <li>
-            Mechanical watches. I own a Seiko SNZG13K1 that I bought for £100,
-            and to this day I just love how clean and classic it looks. Although
-            I wear an Apple Watch daily, I enjoy putting the Seiko on every now
-            and then which does feel good.
-          </li>
-          <li>
-            Vi keybinds, I can&apos;t live without them as it simply makes
-            programming a joy.
-          </li>
-          <li>
-            Consuming informational content at 2x speed.{" "}
-            <a
-              href="https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk?hl=en"
-              rel="prefetch noreferrer"
-              target="_blank"
-            >
-              Video Speed Controller
-            </a>{" "}
-            has completely changed the way I watch videos. I of course watch
-            movies and shows at normal speed, I&apos;m not a psycho.
-          </li>
-          <li>
-            Building things from scratch as opposed to buying it from the shelf.
-            It&apos;s much more fulfilling to enjoy the journey. When something
-            is built, it even has your own battle scars from making some
-            mistakes.
-          </li>
-        </ul>
-        <h2 className="text-2xl font-bold mb-3 mt-8">Technologies</h2>
-        <ul className="gap-y-3 grid grid-cols-2 sm:grid-cols-3 justify-between">
-          <TechItem name="TypeScript" Icon={SiTypescript} />
-          <TechItem name="Node.js" Icon={SiNodedotjs} />
-          <TechItem name="Next.js" Icon={SiNextdotjs} />
-          <TechItem name="Go" Icon={SiGo} />
-          <TechItem name="Java" Icon={SiJava} />
-          <TechItem name="PostgreSQL" Icon={SiPostgresql} />
-          <TechItem name="Neovim" Icon={SiNeovim} />
-          <TechItem name="IntelliJ" Icon={SiIntellijidea} />
-          <TechItem name="VSCode" Icon={SiVisualstudiocode} />
-        </ul>
-        <h2 className="text-2xl font-bold mb-3 mt-8">Setup</h2>
-        <ul className="prose dark:prose-invert list-disc">
-          <li>M1 MacBook Pro 13&quot;.</li>
-          <li>
-            <a
-              href="https://youtu.be/RqktmDE-TGA"
-              rel="prefetch noreferrer"
-              target="_blank"
-            >
-              KBD67 Lite R3
-            </a>
-            .
-          </li>
-          <li>MX Master 3.</li>
-          <li>Lenovo P27h-20 27&quot; Monitor.</li>
-          <li>Ergo Desks Liberty Standing Desk.</li>
-          <li>IKEA JÄRVFJÄLLET Chair.</li>
-          <li>Sony WH-1000XM4 Wireless Noise Cancelling Headphones.</li>
-          <li>DeltaHub Minimalistic Desk Pad.</li>
-          <li>Twelve South Curve MacBook Stand.</li>
-        </ul>
+        <div>
+          <h2 className="text-2xl font-bold mb-3 mt-8">Things I like</h2>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/pzrsa?tab=stars"
+                rel="prefetch noreferrer"
+                target="_blank"
+              >
+                Open source projects
+              </a>
+              . It&apos;s fascinating that random people all over the internet
+              are able to come together to change the world.
+            </li>
+            <li>
+              Reading non-fiction and fiction books on my Kindle. Currently
+              reading{" "}
+              <a
+                href="https://en.wikipedia.org/wiki/The_Obstacle_Is_the_Way"
+                rel="prefetch noreferrer"
+                target="_blank"
+              >
+                The Obstacle Is the Way
+              </a>{" "}
+              by Ryan Holiday.
+            </li>
+            <li>
+              Mechanical keyboards. I love the fact that I could build something
+              with parts tailored to my preference. I use a keyboard daily, so
+              it makes sense for me to invest in tools that inspire me to use
+              them more.
+            </li>
+            <li>
+              Doing typing tests on{" "}
+              <a
+                href="https://monkeytype.com/"
+                rel="prefetch noreferrer"
+                target="_blank"
+              >
+                monkeytype
+              </a>
+              . I&apos;ve now thankfully moved over to using 9 fingers for
+              typing after only using 3 previously. Wasn&apos;t easy but 100%
+              worth the frustration in the beginning. My current highest WPM is
+              116 (95% accuracy).
+            </li>
+            <li>
+              Mechanical watches and G-Shock&apos;s. I own a Seiko SNZG13K1 and
+              a G-Shock GA-B2100-1A1, both of which I bought for under £100. I
+              just love how clean they look and how good it feels wearing them.
+            </li>
+            <li>
+              Vi keybinds, I can&apos;t live without them as it simply makes
+              interacting with the computer a joy.
+            </li>
+            <li>
+              Consuming informational content at 2x speed.{" "}
+              <a
+                href="https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk?hl=en"
+                rel="prefetch noreferrer"
+                target="_blank"
+              >
+                Video Speed Controller
+              </a>{" "}
+              has completely changed the way I watch videos. I of course watch
+              movies and shows at normal speed, I&apos;m not a psycho.
+            </li>
+            <li>
+              Building things from scratch as opposed to buying it from the
+              shelf. It&apos;s much more fulfilling to enjoy the journey. When
+              something is built, it even has your own battle scars from making
+              some mistakes.
+            </li>
+          </ul>
+          <h2 className="text-2xl font-bold mb-3 mt-8">Technologies</h2>
+          <ul className="not-prose gap-y-3 grid grid-cols-2 sm:grid-cols-3 justify-between">
+            <TechItem name="TypeScript" Icon={SiTypescript} />
+            <TechItem name="Node.js" Icon={SiNodedotjs} />
+            <TechItem name="Next.js" Icon={SiNextdotjs} />
+            <TechItem name="Go" Icon={SiGo} />
+            <TechItem name="Java" Icon={SiJava} />
+            <TechItem name="PostgreSQL" Icon={SiPostgresql} />
+            <TechItem name="Neovim" Icon={SiNeovim} />
+            <TechItem name="IntelliJ" Icon={SiIntellijidea} />
+            <TechItem name="VSCode" Icon={SiVisualstudiocode} />
+          </ul>
+          <h2 className="text-2xl font-bold mb-3 mt-8">Setup</h2>
+          <ul className="prose dark:prose-invert">
+            <li>M1 MacBook Pro 13&quot;.</li>
+            <li>
+              <a
+                href="https://youtu.be/RqktmDE-TGA"
+                rel="prefetch noreferrer"
+                target="_blank"
+              >
+                KBD67 Lite R3
+              </a>
+              .
+            </li>
+            <li>MX Master 3.</li>
+            <li>Lenovo P27h-20 27&quot; Monitor.</li>
+            <li>Ergo Desks Liberty Standing Desk.</li>
+            <li>IKEA JÄRVFJÄLLET Chair.</li>
+            <li>Sony WH-1000XM4 Headphones.</li>
+            <li>DeltaHub Minimalistic Desk Pad.</li>
+            <li>Twelve South Curve MacBook Stand.</li>
+          </ul>
+        </div>
       </div>
     </>
   );
