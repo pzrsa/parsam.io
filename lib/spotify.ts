89fee0837a4a57158600565e444f28039c8bafca
parsa <parsamesg@gmail.com>
2022-06-26 18:29:50 +0100

    cleanup deps

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,5 +1,3 @@
-import querystring from "query-string";
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
@@ -16,10 +14,7 @@ const getAccessToken = async () => {
       Authorization: `Basic ${basic}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token,
-    }),
+    body: `grant_type=refresh_token&refresh_token=${refresh_token}`,
   });
 
   return response.json();
