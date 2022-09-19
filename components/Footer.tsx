ce19bf379f62ec58eaa67045949cf39ffe139620
parsa <hi@parsam.io>
2022-09-19 13:20:51 +0100

    remove feeds, need to reimplement later

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 interface FooterProps {
   href: string;
   name: string;
@@ -21,11 +19,6 @@ const FooterItem: React.FC<FooterProps> = ({ href, name }) => {
 const Footer = () => {
   return (
     <footer className="grid grid-cols-3 sm:flex-row sm:flex gap-4 mt-10">
-      <Link href={"/feeds"}>
-        <a className="font-mono font-bold text-md sm:text-lg hover:text-neutral-500 dark:hover:text-neutral-400 transition-all">
-          feeds
-        </a>
-      </Link>
       <FooterItem href="https://github.com/pzrsa/parsam.io" name="source" />
       <FooterItem href="https://twitter.com/_parsam" name="twitter" />
       <FooterItem href="https://github.com/pzrsa" name="github" />
--- lib/constants.ts
+++ lib/constants.ts
@@ -2,6 +2,3 @@ import path from "path";
 
 export const NOTES_DIRECTORY = path.join(process.cwd(), "data/notes");
 export const ARTICLES_DIRECTORY = path.join(process.cwd(), "data/articles");
-export const FEED_FOLDER = "./public";
-export const NOTES_ATOM_FEED = "./public/notes.atom";
-export const ARTICLES_ATOM_FEED = "./public/articles.atom";
