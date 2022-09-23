aa0ebcaea6c4d4ef594f9ae688f066a8a05228c2
parsa <hi@parsam.io>
2022-09-24 00:38:06 +0100

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,16 +22,18 @@ const Index: React.FC = () => {
       </Head>
       <div className="gap-1">
         <h1 className="text-4xl font-extrabold sm:text-5xl">
-          Hey, I&apos;m Parsa.
+          Hi, I&apos;m Parsa.
         </h1>
         <p className="font-mono text-sm sm:text-lg font-semibold text-neutral-600 dark:text-neutral-400">
           Software Engineering Apprentice at{" "}
-          <span className="text-google-blue">G</span>
-          <span className="text-google-red">o</span>
-          <span className="text-google-yellow">o</span>
-          <span className="text-google-blue">g</span>
-          <span className="text-google-green">l</span>
-          <span className="text-google-red">e</span>
+          <span className="font-serif">
+            <span className="text-google-blue">G</span>
+            <span className="text-google-red">o</span>
+            <span className="text-google-yellow">o</span>
+            <span className="text-google-blue">g</span>
+            <span className="text-google-green">l</span>
+            <span className="text-google-red">e</span>
+          </span>
         </p>
         <div className="max-w-fit my-6">
           <NowPlaying />
@@ -40,18 +42,31 @@ const Index: React.FC = () => {
       <div className="prose dark:prose-invert">
         <p>
           I&apos;m a 19 year old from London, England keen on learning deeply
-          about stuff I love including tech, startups and life.
+          about{" "}
+          <Link href="/about">
+            <a>stuff I love</a>
+          </Link>{" "}
+          including tech, startups and life.
         </p>
         <p>
-          I work within the{" "}
+          I currently work within the{" "}
           <a
-            href="https://web.dev/web-on-android/"
+            href="https://www.w3.org"
             rel="prefetch noreferrer"
             target="_blank"
           >
-            Web on Android
+            Web
           </a>{" "}
-          team.
+          on{" "}
+          <a
+            href="https://android.com"
+            rel="prefetch noreferrer"
+            target="_blank"
+          >
+            Android
+          </a>{" "}
+          team, focusing on problems related to the Web ecosystem for Android
+          devices.
         </p>
         <p>
           I write{" "}
