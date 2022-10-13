9659431de4e0099124e62e3852dc30fec9afe9b1
parsa <hi@parsam.io>
2022-10-13 21:46:43 +0100

    replace mr morale with yeezus (sorry kendrick)

--- data/music.ts
+++ data/music.ts
@@ -7,19 +7,14 @@ export const albums: Album[] = [
     url: "https://open.spotify.com/album/6pwuKxMUkNg673KETsXPUV",
   },
   {
+    title: "ye",
+    image: "https://i.scdn.co/image/ab67616d0000b2730cd942c1a864afa4e92d04f2",
+    url: "https://open.spotify.com/album/2Ek1q2haOnxVqhvVKqMvJe",
+  },
+  {
     title: "The Life Of Pablo",
     image: "https://i.scdn.co/image/ab67616d0000b2732a7db835b912dc5014bd37f4",
     url: "https://open.spotify.com/album/7gsWAHLeT0w7es6FofOXk1",
-  },
-  {
-    title: "Mr. Morale & The Big Steppers",
-    image: "https://i.scdn.co/image/ab67616d0000b273f30764d5fa1e439ad6a7742f",
-    url: "https://open.spotify.com/album/1atjqOZTCdrjxjMyCPZc2g",
-  },
-  {
-    title: "My Beautiful Dark Twisted Fantasy",
-    image: "https://i.scdn.co/image/ab67616d0000b273d9194aa18fa4c9362b47464f",
-    url: "https://open.spotify.com/album/20r762YmB5HeofjMCiPMLv",
   },
   {
     title: "Graduation",
@@ -27,8 +22,13 @@ export const albums: Album[] = [
     url: "https://open.spotify.com/album/4SZko61aMnmgvNhfhgTuD3",
   },
   {
-    title: "ye",
-    image: "https://i.scdn.co/image/ab67616d0000b2730cd942c1a864afa4e92d04f2",
-    url: "https://open.spotify.com/album/2Ek1q2haOnxVqhvVKqMvJe",
+    title: "My Beautiful Dark Twisted Fantasy",
+    image: "https://i.scdn.co/image/ab67616d0000b273d9194aa18fa4c9362b47464f",
+    url: "https://open.spotify.com/album/20r762YmB5HeofjMCiPMLv",
+  },
+  {
+    title: "Yeezus",
+    image: "https://i.scdn.co/image/ab67616d0000b2731dacfbc31cc873d132958af9",
+    url: "https://open.spotify.com/album/7D2NdGvBHIavgLhmcwhluK?si=MfXgC_ZMRTWFslTpw-1Q3Q",
   },
 ];
