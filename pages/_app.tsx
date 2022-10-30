83d91be38da9a606a24927e1441bd56f6a4ec56a
parsa <hi@parsam.io>
2022-10-30 17:16:56 +0000

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 };
