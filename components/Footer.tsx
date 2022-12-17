8f3019202abe23054970d2b71e0733b96f241f5f
parsa <hi@parsam.io>
2022-12-17 23:30:08 +0000

    swap order of footer around

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,10 +19,10 @@ const FooterItem: React.FC<FooterProps> = ({ href, name }) => {
 const Footer = () => {
   return (
     <footer className="grid grid-cols-3 sm:flex-row sm:flex gap-4 mt-10">
-      <FooterItem href="https://github.com/pzrsa/parsam.io" name="source" />
-      <FooterItem href="https://twitter.com/pzrsaa" name="twitter" />
       <FooterItem href="https://github.com/pzrsa" name="github" />
+      <FooterItem href="https://twitter.com/pzrsaa" name="twitter" />
       <FooterItem href="https://www.youtube.com/@pzrsa" name="youtube" />
+      <FooterItem href="https://github.com/pzrsa/parsam.io" name="source" />
     </footer>
   );
 };
