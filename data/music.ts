abc704812ea81c277fcffb580056084e760f9eb9
parsa <hi@parsam.io>
2022-12-20 21:46:45 +0000

    move albums around

--- data/music.ts
+++ data/music.ts
@@ -2,24 +2,14 @@ import { Album } from "../lib/types";
 
 export const albums: Album[] = [
   {
+    title: "Yeezus",
+    image: "https://i.scdn.co/image/ab67616d0000b2731dacfbc31cc873d132958af9",
+    url: "https://open.spotify.com/album/7D2NdGvBHIavgLhmcwhluK?si=MfXgC_ZMRTWFslTpw-1Q3Q",
+  },
+  {
     title: "KIDS SEE GHOSTS",
     image: "https://i.scdn.co/image/ab67616d0000b273013c00ee367dd85396f79c82",
     url: "https://open.spotify.com/album/6pwuKxMUkNg673KETsXPUV",
-  },
-  {
-    title: "ye",
-    image: "https://i.scdn.co/image/ab67616d0000b2730cd942c1a864afa4e92d04f2",
-    url: "https://open.spotify.com/album/2Ek1q2haOnxVqhvVKqMvJe",
-  },
-  {
-    title: "The Life Of Pablo",
-    image: "https://i.scdn.co/image/ab67616d0000b2732a7db835b912dc5014bd37f4",
-    url: "https://open.spotify.com/album/7gsWAHLeT0w7es6FofOXk1",
-  },
-  {
-    title: "Graduation",
-    image: "https://i.scdn.co/image/ab67616d0000b27326f7f19c7f0381e56156c94a",
-    url: "https://open.spotify.com/album/4SZko61aMnmgvNhfhgTuD3",
   },
   {
     title: "My Beautiful Dark Twisted Fantasy",
@@ -27,8 +17,18 @@ export const albums: Album[] = [
     url: "https://open.spotify.com/album/20r762YmB5HeofjMCiPMLv",
   },
   {
-    title: "Yeezus",
-    image: "https://i.scdn.co/image/ab67616d0000b2731dacfbc31cc873d132958af9",
-    url: "https://open.spotify.com/album/7D2NdGvBHIavgLhmcwhluK?si=MfXgC_ZMRTWFslTpw-1Q3Q",
+    title: "ye",
+    image: "https://i.scdn.co/image/ab67616d0000b2730cd942c1a864afa4e92d04f2",
+    url: "https://open.spotify.com/album/2Ek1q2haOnxVqhvVKqMvJe",
+  },
+  {
+    title: "Graduation",
+    image: "https://i.scdn.co/image/ab67616d0000b27326f7f19c7f0381e56156c94a",
+    url: "https://open.spotify.com/album/4SZko61aMnmgvNhfhgTuD3",
+  },
+  {
+    title: "The Life Of Pablo",
+    image: "https://i.scdn.co/image/ab67616d0000b2732a7db835b912dc5014bd37f4",
+    url: "https://open.spotify.com/album/7gsWAHLeT0w7es6FofOXk1",
   },
 ];
