c36e41f0d6b74af689dee215857b09288686ae51
parsa <hi@parsam.io>
2022-12-28 12:56:23 +0000

    increase global font size once again

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         DEFAULT: {
           css: {
             color: theme("colors.black"),
-            fontSize: rem(17),
+            fontSize: rem(18),
             a: {
               color: theme("colors.blue.600"),
               fontWeight: "700",
@@ -39,7 +39,7 @@ module.exports = {
         },
         invert: {
           css: {
-            fontSize: rem(17),
+            fontSize: rem(18),
             color: theme("colors.white"),
             a: {
               color: theme("colors.blue.500"),
