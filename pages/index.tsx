f38e7a03ef2662aaf074f567b2f2f5cbd715c4c0
parsa <hi@parsam.io>
2023-04-30 02:01:40 +0100

    add list of posts on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
-import { GetStaticProps } from "next";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import type { GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import Date from "../components/Date";
 import NowPlaying from "../components/NowPlaying";
-import { ARTICLES_DIRECTORY, NOTES_DIRECTORY } from "../lib/constants";
-import { getLatestPost } from "../lib/posts";
+import { POSTS_DIRECTORY } from "../lib/constants";
+import { generateFeed } from "../lib/feeds";
+import { getSortedPostData } from "../lib/posts";
 import { Post } from "../lib/types";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const latestBookNote = getLatestPost(NOTES_DIRECTORY);
-  const latestArticle = getLatestPost(ARTICLES_DIRECTORY);
+  generateFeed();
+  const posts = getSortedPostData(POSTS_DIRECTORY);
   return {
     props: {
-      latestBookNote,
-      latestArticle,
+      posts,
     },
   };
 };
 
 interface IndexProps {
-  latestBookNote: Post;
-  latestArticle: Post;
+  posts: Post[];
 }
 
-const Index: React.FC<IndexProps> = ({ latestBookNote, latestArticle }) => {
+const Index: React.FC<IndexProps> = ({ posts }) => {
   const image = "index.jpg";
 
   return (
@@ -86,39 +86,46 @@ const Index: React.FC<IndexProps> = ({ latestBookNote, latestArticle }) => {
           reason), check out my <Link href="/about">about page</Link>.
         </p>
         <p>
-          On this website, I write <Link href="/notes">notes</Link> on the books
-          I&apos;ve read, providing summaries for others with my thoughts on the
-          book. I also write <Link href="/articles">articles</Link> on stuff I
-          find interesting enough to share.
+          On this website, I write notes on the books I&apos;ve read, providing
+          summaries with my thoughts on the book. I also write articles on stuff
+          I find interesting enough to share.
         </p>
       </div>
       <div>
-        <h1 className="text-3xl sm:text-4xl font-bold my-4">
-          Latest Book Note
-        </h1>
-        <Link
-          href={`/notes/${latestBookNote.slug}`}
-          className="hover:text-neutral-500 dark:hover:text-neutral-400 transition-all text-2xl sm:text-3xl font-bold"
-        >
-          {latestBookNote.title}
-        </Link>
-        <p className="font-mono text-neutral-600 dark:text-neutral-400">
-          {latestBookNote.author} • {latestBookNote.genre} •{" "}
-          <Date dateString={latestBookNote.date} />
-        </p>
-        <p className="mt-2">{latestBookNote.description}</p>
-      </div>
-      <div>
-        <h1 className="text-3xl sm:text-4xl font-bold my-4">Latest Article</h1>
-        <Link
-          href={`/articles/${latestArticle.slug}`}
-          className="hover:text-neutral-500 dark:hover:text-neutral-400 transition-all text-2xl sm:text-3xl font-bold"
-        >
-          {latestArticle.title}
-        </Link>
-        <p className="font-mono text-neutral-600 dark:text-neutral-400 mt-1">
-          <Date dateString={latestArticle.date} />
-        </p>
+        <h1 className="text-2xl sm:text-3xl font-bold my-2">Posts</h1>
+        <ul>
+          {posts.map((post, i) => {
+            dayjs.extend(customParseFormat);
+            const date = dayjs(post.date).format("D MMM");
+            const year = new Date(post.date).getFullYear();
+            const firstForYear =
+              !posts[i - 1] ||
+              new Date(posts[i - 1].date).getFullYear() !== year;
+            const lastForYear =
+              !posts[i + 1] ||
+              new Date(posts[i + 1].date).getFullYear() !== year;
+
+            return (
+              <li key={post.id}>
+                <span className="flex justify-end mb-1">
+                  {firstForYear && <span className="font-bold">{year}</span>}
+                </span>
+                <Link href={`/${post.id}`} className="group">
+                  <span
+                    className={`flex items-center group-hover:text-neutral-500 dark:group-hover:text-neutral-400 transition-all ${
+                      lastForYear ? "mb-4" : ""
+                    }`}
+                  >
+                    <span className="flex-1 text-lg font-semibold">
+                      {post.title}
+                    </span>
+                    <span className="font-mono">{date}</span>
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
