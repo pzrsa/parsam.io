5941e5ba7311696c493b6d7a1aedfe6edecec193
parsa <hi@parsam.io>
2023-04-30 02:03:10 +0100

    setup redirects to new path

--- next.config.js
+++ next.config.js
@@ -3,4 +3,28 @@ module.exports = {
   images: {
     domains: ["i.scdn.co"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/notes",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/articles",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/notes/:id",
+        destination: "/:id",
+        permanent: true,
+      },
+      {
+        source: "/articles/:id",
+        destination: "/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
