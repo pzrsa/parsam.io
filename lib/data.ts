dc32b28748600fb80e0402805bd759aa09c36cdd
parsa <hi@parsam.io>
2023-08-26 21:20:56 +0100

    update films

--- lib/data.ts
+++ lib/data.ts
@@ -7,19 +7,9 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt4633694",
   },
   {
-    title: "Spider-Man: Across the Spider-Verse",
-    imageID: "8Vt6mWEReuy4Of61Lnj5Xj704m8",
-    imdbID: "tt9362722",
-  },
-  {
-    title: "Taxi Driver",
-    imageID: "ekstpH614fwDX8DUln1a2Opz0N8",
-    imdbID: "tt0075314",
-  },
-  {
-    title: "The Social Network",
-    imageID: "n0ybibhJtQ5icDqTp8eRytcIHJx",
-    imdbID: "tt1285016",
+    title: "La La Land",
+    imageID: "uDO8zWDhfWwoFdKS4fzkUJt0Rf0",
+    imdbID: "tt3783958",
   },
   {
     title: "The Dark Knight",
@@ -27,19 +17,44 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt0468569",
   },
   {
+    title: "The Social Network",
+    imageID: "n0ybibhJtQ5icDqTp8eRytcIHJx",
+    imdbID: "tt1285016",
+  },
+  {
+    title: "Taxi Driver",
+    imageID: "ekstpH614fwDX8DUln1a2Opz0N8",
+    imdbID: "tt0075314",
+  },
+  {
     title: "Fight Club",
     imageID: "pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK",
     imdbID: "tt0137523",
   },
   {
-    title: "La La Land",
-    imageID: "uDO8zWDhfWwoFdKS4fzkUJt0Rf0",
-    imdbID: "tt3783958",
+    title: "Whiplash",
+    imageID: "7fn624j5lj3xTme2SgiLCeuedmO",
+    imdbID: "tt2582802",
   },
   {
     title: "Baby Driver",
     imageID: "rmnQ9jKW72bHu8uKlMjPIb2VLMI",
     imdbID: "tt3890160",
+  },
+  {
+    title: "Spider-Man: Across the Spider-Verse",
+    imageID: "8Vt6mWEReuy4Of61Lnj5Xj704m8",
+    imdbID: "tt9362722",
+  },
+  {
+    title: "The Dark Knight Rises",
+    imageID: "hr0L2aueqlP2BYUblTTjmtn0hw4",
+    imdbID: "tt1345836",
+  },
+  {
+    title: "Oppenheimer",
+    imageID: "8Gxv8gSFCU0XGDykEGv7zR1n2ua",
+    imdbID: "tt15398776",
   },
   {
     title: "Her",
@@ -57,34 +72,9 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt0110912",
   },
   {
-    title: "How to Train Your Dragon",
-    imageID: "ygGmAO60t8GyqUo9xYeYxSZAR3b",
-    imdbID: "tt0892769",
-  },
-  {
     title: "Inglourious Basterds",
     imageID: "7sfbEnaARXDDhKm0CZ7D7uc2sbo",
     imdbID: "tt0361748",
-  },
-  {
-    title: "Superbad",
-    imageID: "ek8e8txUyUwd2BNqj6lFEerJfbq",
-    imdbID: "tt0829482",
-  },
-  {
-    title: "Scott Pilgrim vs. the World",
-    imageID: "g5IoYeudx9XBEfwNL0fHvSckLBz",
-    imdbID: "tt0446029",
-  },
-  {
-    title: "Mad Max: Fury Road",
-    imageID: "hA2ple9q4qnwxp3hKVNhroipsir",
-    imdbID: "tt1392190",
-  },
-  {
-    title: "Drive",
-    imageID: "602vevIURmpDfzbnv5Ubi6wIkQm",
-    imdbID: "tt0780504",
   },
   {
     title: "Harry Potter and the Deathly Hallows: Part 2",
@@ -92,9 +82,19 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt1201607",
   },
   {
-    title: "American Psycho",
-    imageID: "9uGHEgsiUXjCNq8wdq4r49YL8A1",
-    imdbID: "tt0144084",
+    title: "Drive",
+    imageID: "602vevIURmpDfzbnv5Ubi6wIkQm",
+    imdbID: "tt0780504",
+  },
+  {
+    title: "Superbad",
+    imageID: "ek8e8txUyUwd2BNqj6lFEerJfbq",
+    imdbID: "tt0829482",
+  },
+  {
+    title: "Mad Max: Fury Road",
+    imageID: "hA2ple9q4qnwxp3hKVNhroipsir",
+    imdbID: "tt1392190",
   },
   {
     title: "No Country for Old Men",
@@ -102,9 +102,24 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt0477348",
   },
   {
-    title: "Blade Runner",
-    imageID: "63N9uy8nd9j7Eog2axPQ8lbr3Wj",
-    imdbID: "tt0083658",
+    title: "Midsommar",
+    imageID: "7LEI8ulZzO5gy9Ww2NVCrKmHeDZ",
+    imdbID: "tt8772262",
+  },
+  {
+    title: "Scott Pilgrim vs. the World",
+    imageID: "g5IoYeudx9XBEfwNL0fHvSckLBz",
+    imdbID: "tt0446029",
+  },
+  {
+    title: "How to Train Your Dragon",
+    imageID: "ygGmAO60t8GyqUo9xYeYxSZAR3b",
+    imdbID: "tt0892769",
+  },
+  {
+    title: "American Psycho",
+    imageID: "9uGHEgsiUXjCNq8wdq4r49YL8A1",
+    imdbID: "tt0144084",
   },
   {
     title: "Paddington 2",
@@ -112,49 +127,14 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt4468740",
   },
   {
-    title: "Midsommar",
-    imageID: "7LEI8ulZzO5gy9Ww2NVCrKmHeDZ",
-    imdbID: "tt8772262",
-  },
-  {
-    title: "The Devil Wears Prada",
-    imageID: "8912AsVuS7Sj915apArUFbv6F9L",
-    imdbID: "tt0458352",
-  },
-  {
-    title: "Get Out",
-    imageID: "mE24wUCfjK8AoBBjaMjho7Rczr7",
-    imdbID: "tt5052448",
-  },
-  {
-    title: "Fantastic Mr. Fox",
-    imageID: "jGhVauMEznd1R0yI6CYG9PONA9D",
-    imdbID: "tt0432283",
-  },
-  {
     title: "Django Unchained",
     imageID: "7oWY8VDWW7thTzWh3OKYRkWUlD5",
     imdbID: "tt1853728",
   },
   {
-    title: "Blade Runner 2049",
-    imageID: "gajva2L0rPYkEWjzgFlBXCAVBE5",
-    imdbID: "tt1856101",
-  },
-  {
     title: "Parasite",
     imageID: "7IiTTgloJzvGI1TAYymCfbfl3vT",
     imdbID: "tt6751668",
-  },
-  {
-    title: "Oppenheimer",
-    imageID: "8Gxv8gSFCU0XGDykEGv7zR1n2ua",
-    imdbID: "tt15398776",
-  },
-  {
-    title: "Whiplash",
-    imageID: "7fn624j5lj3xTme2SgiLCeuedmO",
-    imdbID: "tt2582802",
   },
   {
     title: "Spirited Away",
@@ -167,19 +147,19 @@ export const FAVOURITE_FILMS: Film[] = [
     imdbID: "tt6791350",
   },
   {
-    title: "Steve Jobs",
-    imageID: "92vhEJ6xWoDJ8UHECAJaRofaSCZ",
-    imdbID: "tt2080374",
-  },
-  {
     title: "The Batman",
     imageID: "74xTEgt7R36Fpooo50r9T25onhq",
     imdbID: "tt1877830",
   },
   {
-    title: "Kung Fu Panda 2",
-    imageID: "mtqqD00vB4PGRt20gWtGqFhrkd0",
-    imdbID: "tt1302011",
+    title: "The Notebook",
+    imageID: "rNzQyW4f8B8cQeg7Dgj3n6eT5k9",
+    imdbID: "tt0332280",
+  },
+  {
+    title: "The Devil Wears Prada",
+    imageID: "8912AsVuS7Sj915apArUFbv6F9L",
+    imdbID: "tt0458352",
   },
 ];
 
