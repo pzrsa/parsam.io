d7ed24e7131e534be441870383d0655538abf2d3
Parsa Mesgarha <pmesgarha@tesla.com>
2025-03-24 19:15:26 +0000

    update open graph

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,6 +1,26 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+async function fetchFont(): Promise<ArrayBuffer | null> {
+  const API = "https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700";
+
+  const css = await (
+    await fetch(API, {
+      headers: {
+        // Make sure it returns TTF (https://stackoverflow.com/questions/46321921/how-to-specify-return-type-format-for-google-fonts).
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/
+  );
+
+  const res = await fetch(resource![1]);
+
+  return res.arrayBuffer();
+}
 
 export async function GET(request: Request) {
   try {
@@ -11,56 +31,56 @@ export async function GET(request: Request) {
       ? searchParams.get("title")?.slice(0, 100)
       : "parsam.io";
 
+    const font = await fetchFont();
+
     return new ImageResponse(
       (
         <div
           style={{
-            background: "white",
-            display: "flex",
             height: "100%",
             width: "100%",
+            display: "flex",
+            flexDirection: "column",
             alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#fff",
+            fontFamily: "Courier Prime",
           }}
         >
           <div
             style={{
-              display: "flex",
-              flexWrap: "wrap",
-              padding: "20px 50px",
-              margin: "0 42px",
-              fontSize: 80,
-              borderRadius: "2px",
-              backgroundColor: "#111",
-              color: "white",
-              lineHeight: 1.4,
+              textAlign: "center",
+              textWrap: "balance",
+              fontSize: 64,
+              marginBottom: 40,
+              textTransform: "uppercase",
             }}
           >
             {title}
           </div>
           <div
             style={{
-              left: 42,
-              bottom: 42,
+              fontSize: 32,
+              color: "#666",
               position: "absolute",
-              display: "flex",
-              alignItems: "center",
+              bottom: 40,
             }}
           >
-            <span
-              style={{
-                fontSize: 60,
-                margin: "0 42px",
-              }}
-            >
-              parsam.io
-            </span>
+            parsam.io
           </div>
         </div>
       ),
       {
         width: 1200,
         height: 630,
-      },
+        fonts: [
+          {
+            name: "Courier Prime",
+            data: font!,
+            style: "normal",
+          },
+        ],
+      }
     );
   } catch (e: any) {
     console.log(`${e.message}`);
