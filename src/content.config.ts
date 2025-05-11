712068d4afa900d2e7b9d298e99c9c2b8b71e486
parsa <hi@parsam.io>
2025-05-11 17:47:23 +0200

    add albums/shows/films

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,8 @@
 import { defineCollection, getCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/data" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
   schema: z.object({
     date: z.coerce.date(),
     title: z.string(),
@@ -11,7 +11,36 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const albums = defineCollection({
+  loader: file("src/data/favourites.json", {
+    parser: (text) => JSON.parse(text).albums,
+  }),
+  schema: z.object({
+    title: z.string(),
+    imageID: z.string(),
+  }),
+});
+
+const filmShowSchema = z.object({
+  title: z.string(),
+  imageID: z.string(),
+});
+
+const films = defineCollection({
+  loader: file("src/data/favourites.json", {
+    parser: (text) => JSON.parse(text).films,
+  }),
+  schema: filmShowSchema,
+});
+
+const shows = defineCollection({
+  loader: file("src/data/favourites.json", {
+    parser: (text) => JSON.parse(text).shows,
+  }),
+  schema: filmShowSchema,
+});
+
+export const collections = { blog, albums, films, shows };
 
 export const getBlogPosts = async () => {
   const posts = await getCollection("blog");
