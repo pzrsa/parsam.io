9c212233859cec3e9356a472d927074993d8400b
parsa <hi@parsam.io>
2025-05-16 11:23:26 +0100

    nothing rly

--- src/pages/api/now-playing.ts
+++ src/pages/api/now-playing.ts
@@ -7,19 +7,19 @@ const NOW_PLAYING_URL =
   "https://api.spotify.com/v1/me/player/currently-playing";
 const TOKEN_URL = "https://accounts.spotify.com/api/token";
 
-export const GET: APIRoute = async () => {
-  const client_id = import.meta.env.SPOTIFY_CLIENT_ID;
-  const client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
-  const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN;
+const CLIENT_ID = import.meta.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.SPOTIFY_CLIENT_SECRET;
+const REFRESH_TOKEN = import.meta.env.SPOTIFY_REFRESH_TOKEN;
 
-  const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+export const GET: APIRoute = async () => {
+  const basic = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
   const tokenRes = await fetch(TOKEN_URL, {
     method: "POST",
     headers: {
       Authorization: `Basic ${basic}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: `grant_type=refresh_token&refresh_token=${refresh_token}`,
+    body: `grant_type=refresh_token&refresh_token=${REFRESH_TOKEN}`,
   });
 
   const tokenData = await tokenRes.json();
